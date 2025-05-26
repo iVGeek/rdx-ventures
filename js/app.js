1b631915712b5a3090142fbcdb3cfb58f3bd431a
iVGeek <ianvincent916@gmail.com>
2025-05-26 13:26:45 +0300

    features

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Main JavaScript file for rdx-ventures
+
+// Example: Initialize ScrollReveal if available
+if (typeof ScrollReveal !== 'undefined') {
+    ScrollReveal().reveal('.reveal', { delay: 200 });
+}
